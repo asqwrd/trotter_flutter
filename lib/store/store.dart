4966cd21a6c7408e70b54ce30bfab0eaf6421a59
Ajibade Adebayo <asqwrd@gmail.com>
2019-07-11 23:39:04 -0700

    notifications ui

--- lib/store/store.dart
+++ lib/store/store.dart
@@ -3,7 +3,8 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:trotter_flutter/store/auth.dart';
 import 'package:trotter_flutter/store/itineraries/store.dart';
-
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'trips/store.dart';
 
 class TrotterStore extends Store {
@@ -15,7 +16,11 @@ class TrotterStore extends Store {
   ItineraryStore itineraryStore = ItineraryStore();
   bool bottomSheetLoading = false;
   bool tripsLoading = false;
+  bool notificationsLoading = false;
   bool tripsRefreshing = false;
+  NotificationsData _notifications =
+      NotificationsData(notifications: [], success: false);
+  get notifications => _notifications;
 
   bool offline = false;
 
@@ -40,6 +45,24 @@ class TrotterStore extends Store {
   setBottomSheetLoading(bool value) {
     setState(() {
       bottomSheetLoading = value;
+    });
+  }
+
+  setNotifications(dynamic data) {
+    setState(() {
+      _notifications = NotificationsData(notifications: data, success: true);
+    });
+  }
+
+  setNotificationsError(bool error) {
+    setState(() {
+      _notifications = NotificationsData(notifications: [], success: false);
+    });
+  }
+
+  setNotificationsLoading(bool loading) {
+    setState(() {
+      notificationsLoading = loading;
     });
   }
 
@@ -85,5 +108,41 @@ class TrotterStore extends Store {
       print('checkstatus error');
       print(error);
     }
+  }
+}
+
+class NotificationsData {
+  final List<dynamic> notifications;
+  final bool success;
+
+  NotificationsData({this.notifications, this.success});
+
+  factory NotificationsData.fromJson(Map<String, dynamic> json) {
+    return NotificationsData(
+        notifications: json['notifications'], success: true);
+  }
+}
+
+Future<NotificationsData> fetchNotifications([TrotterStore store]) async {
+  try {
+    final response = await http.get(
+        'http://localhost:3002/api/notifications?user_id=${store.currentUser.uid}',
+        headers: {'Authorization': 'security'});
+    if (response.statusCode == 200) {
+      // If server returns an OK response, parse the JSON
+      var results = NotificationsData.fromJson(json.decode(response.body));
+      store?.setNotificationsError(false);
+      store?.setOffline(false);
+      store?.setNotifications(results.notifications);
+      store?.setNotificationsLoading(false);
+      return results;
+    } else {
+      // If that response was not OK, throw an error.
+      return NotificationsData(success: false);
+    }
+  } catch (error) {
+    store?.setNotificationsError(true);
+    store?.setNotificationsLoading(false);
+    return NotificationsData(success: false);
   }
 }

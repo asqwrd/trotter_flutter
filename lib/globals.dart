64cfbb4203e5b021da5e8927bb3da8c7f123eb59
Ajibade Adebayo <bade.adebayo@uber.com>
2019-10-04 15:25:48 -0700

    reverted global

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,7 +1,7 @@
 library trotter.globals;
 
 const isRelease = bool.fromEnvironment("dart.vm.product");
-const ApiDomain = isRelease == false
+const ApiDomain = isRelease == true
     ? 'http://trotter-api.ajibade.me'
     : 'http://localhost:3002';
 const GOOGLE_API_KEY = 'AIzaSyBGMENZRkGzoOY0DiRgq3dBuJ1OcEFjlPA';

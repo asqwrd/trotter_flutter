5a2483e4b73695540206fc1dd1ac670d84c81282
Ajibade Adebayo <asqwrd@gmail.com>
2019-12-25 09:41:51 -0800

    changed api to use https

--- lib/globals.dart
+++ lib/globals.dart
@@ -2,6 +2,6 @@ library trotter.globals;
 
 const isRelease = bool.fromEnvironment("dart.vm.product");
 //const ApiDomain = 'http://localhost:3002'; // uncomment to point to localhost
-const ApiDomain = 'http://trotter-api.ajibade.me';
+const ApiDomain = 'https://trotter-api.ajibade.me';
 const GOOGLE_API_KEY = 'AIzaSyBGMENZRkGzoOY0DiRgq3dBuJ1OcEFjlPA';
 const APITOKEN = 'UTZi2D2t7rnFyMXGNURF';
